7fdcf7d08c43a5df443a5acd6e61aec7205357ed
austincap <austin.capobianco@gmail.com>
2024-05-07 20:52:22 -0400

    points working

---
+++ PictureElements/People/CommonPeople1.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+signal capturedImage
+var points = 5
+var picpoints = [false, false, false]
+func checkpoints():
+	await get_tree().create_timer(0.1).timeout
+	if false in picpoints:
+		print("COPE")
+	else:
+		emit_signal("capturedImage", self)
+	picpoints = [false, false, false]
+
+func _on_target_1_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	picpoints[0] = true
+	checkpoints()
+
+
+func _on_target_2_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	picpoints[1] = true
+	checkpoints()
+
+
+func _on_corearea_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	picpoints[2] = true
+	checkpoints()
--- testworld.gd
+++ testworld.gd
@@ -5,16 +5,19 @@ extends Node2D
 # var a = 2
 var speed = 100
 var selecteditem = []
+var serv
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var CP1 = get_node("CommonPeople1")
+	CP1.connect("capturedImage", captured)
 	var tween = create_tween()
 	tween.tween_property($Heart,"position:x", 10, 600)
 	var tween2 = create_tween()
 	tween2.tween_property($Bird, "position", Vector2(2000, 0), 1.0)
 	#add_child($Tween)
 	#tween.start()
-
+	serv = PhysicsServer2D
 
 func _on_Tween_tween_completed(_object, _key):
 	var tween = create_tween()
@@ -29,16 +32,51 @@ func _on_Tween_tween_completed(_object, _key):
 func _process(delta):
 	$Path2D/PathFollow2D.loop = true
 	$Path2D/PathFollow2D.progress+= speed*delta
+	
+func _physics_process(delta):
+	pass
 
+func captured(entity):
+	print(entity.get_meta("points"))
+	var GUI = get_node("GUI")
+	GUI.global_position = entity.global_transform.origin
+	GUI.get_node("Label").text = str("+", entity.get_meta("points"))
+	await get_tree().create_timer(1.0).timeout
+	GUI.get_node("Label").text = ""
 
 func _on_area_2d_area_entered(area):
 	selecteditem.append(area)
-	print("AREA ENTEREd")
+	#print("AREA ENTEREd")
+
+func picturesnapped(area):
+	if (area.is_in_group("target")):
+		print("TARGET")
+	if (area.is_in_group("core")):
+		print("CORE")
+		print(area.get_name())
 
 
 func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	print("COMMONER AREASHAPE ENTERED")
-	print(area.get_name())
+	#	var space := serv.Physics2DDirectSpaceState
+	#	var query = area.get_child(0).get_shape()
+	#	#query.collide_with_areas = true
+	#	#query.collide_with_bodies = false
+	#	#query.set_shape($CollisionShape2D.shape)
+	#	#query.transform = self.global_transform
+	#	var result := space.intersect_shape(query)
+	#	for area_collision in result:
+	#		var areas := area_collision.collider as Area2D
+	#		if is_instance_valid(area): #perhaps some other checks here
+	#			print("Overlapping " + areas.name)
+	print("CRAP")
+	print(area.get_overlapping_areas())
+	print(area.get_parent().get_name())
+	print(area.get_child(0))
+	if (area.is_in_group("target")):
+		print("TARGET")
+	if (area.is_in_group("core")):
+		print("CORE")
+		print(area.get_name())
 	#print(local_shape_index)
 	#print(area.get_meta("points"))
 	#print(area.get_parent().get_name())
